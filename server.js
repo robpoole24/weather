76bf56e22842e7b8d5bcf6bdb19e618f7cd86071
robpoole24 <robpoole24@gmail.com>
2026-05-26 19:34:31 -0500

    Changed card wrappers

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-// WeatherTV Server — updated 2026-05-26 build.1779841336
+// WeatherTV Server — updated 2026-05-26 build.1779841950
 const express = require('express');
 const cors = require('cors');
 const fs = require('fs');
